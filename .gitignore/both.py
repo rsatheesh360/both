a8d077d53c74b87d8b812092a627b231e45c3522
R.Satheesh <36471895+rsatheesh360@users.noreply.github.com>
2018-03-15 17:09:16 +0530

    alpha and num

---
+++ .gitignore/both.py
@@ -0,0 +1,6 @@
+n =(input("enter the something:"))
+if (n==int or n==str):
+    print(n,("contains both alphabet and numeric values"))
+else:
+    print(("enter both alphabets and numbers"))
+    print(n)
